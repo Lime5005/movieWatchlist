dff8a313542f7491acda9eb0770319e13037a2a5
Lime5005 <zlp5005@gmail.com>
2021-08-31 15:55:31 +0200

    Work flow: view-controller-service-repository

--- src/main/java/com/openclassrooms/watchlist/controller/WatchlistController.java
+++ src/main/java/com/openclassrooms/watchlist/controller/WatchlistController.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.watchlist.controller;
 
 import com.openclassrooms.watchlist.domain.WatchlistItem;
+import com.openclassrooms.watchlist.exception.DuplicateTitleException;
+import com.openclassrooms.watchlist.service.WatchlistService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
 public class WatchlistController {
-
-    private final List<WatchlistItem> watchlistItemLists = new ArrayList<>();
-    private static int index = 1;
+    WatchlistService watchlistService = new WatchlistService();
 
     @GetMapping("/watchlistItemForm")
     public ModelAndView showWatchlistItemForm(@RequestParam(required = false) Integer id) {
         String viewName = "watchlistItemForm";
         Map<String, Object> model = new HashMap<>();
-        WatchlistItem watchlistItem = findWatchlistItemById(id);
+        WatchlistItem watchlistItem = watchlistService.findWatchlistItemById(id);
         if (watchlistItem == null) {
             model.put("watchlistItem", new WatchlistItem());
         } else {
@@ -34,39 +32,17 @@ public class WatchlistController {
         return new ModelAndView(viewName, model);
     }
 
-    private WatchlistItem findWatchlistItemById(Integer id) {
-        // Loop through all the list to find the item:
-        for (WatchlistItem watchlistItem : watchlistItemLists) {
-            if(watchlistItem.getId().equals(id)) {
-                return watchlistItem;
-            }
-        }
-        return null;
-    }
-
-
     @PostMapping("/watchlistItemForm")
     public ModelAndView submitWatchlistFrom(@Valid WatchlistItem watchlistItem, BindingResult bindingResult) {
         // If valid is not fully accomplished, return a blank form:
         if (bindingResult.hasErrors()) {
             return new ModelAndView("watchlistItemForm");
         }
-        WatchlistItem item = findWatchlistItemById(watchlistItem.getId());
-        if (item == null) {
-            // If it's a new id, then check the given title if it's also new:
-            if (titleAlreadyExists(watchlistItem.getTitle())) {
-                bindingResult.rejectValue("title", "", "This title already exists on your watchlist.");
-                return new ModelAndView("watchlistItemForm");
-            }
-            watchlistItem.setId(index++);
-            watchlistItemLists.add(watchlistItem);
-        } else {
-            item.setTitle(watchlistItem.getTitle());
-            item.setRating(watchlistItem.getRating());
-            item.setPriority(watchlistItem.getPriority());
-            item.setComment(watchlistItem.getComment());
-            // Note that the id should be original, how to find it? Set a hidden input field.
-
+        try {
+            watchlistService.addOrUpdateWatchlistItem(watchlistItem);
+        } catch (DuplicateTitleException e) {
+            bindingResult.rejectValue("title", "", "This title already exists on your watchlist.");
+            return new ModelAndView("watchlistItemForm");
         }
 
         RedirectView redirectView = new RedirectView();
@@ -75,28 +51,12 @@ public class WatchlistController {
         return new ModelAndView(redirectView);
     }
 
-    private boolean titleAlreadyExists(String title) {
-        for (WatchlistItem item : watchlistItemLists) {
-            if (item.getTitle().equals(title)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     @GetMapping("/watchlist")
     public ModelAndView getWatchlist() {
-
-        /*watchlistItemLists.clear();// Clear previous call.
-        watchlistItemLists.add(new WatchlistItem("Lion King","8.5", "high","Very good!", index++ ));
-        watchlistItemLists.add(new WatchlistItem("Back to the future","9.5", "high","Very good!", index++));
-        watchlistItemLists.add(new WatchlistItem("Sex and the city","7.5", "high","Very good!", index++));
-        watchlistItemLists.add(new WatchlistItem("Emily in Paris","8.5", "high","Very good!", index++));
-*/
         String viewName = "watchlist";
         Map<String, Object> model = new HashMap<>();
-        model.put("watchlistItems", watchlistItemLists);
-        model.put("numberOfMovies", watchlistItemLists.size());
+        model.put("watchlistItems", watchlistService.getWatchlistItems());
+        model.put("numberOfMovies", watchlistService.getWatchlistItemsSize());
         return new ModelAndView(viewName, model);
     }
 }
--- src/main/java/com/openclassrooms/watchlist/service/WatchlistService.java
+++ src/main/java/com/openclassrooms/watchlist/service/WatchlistService.java
@@ -1,5 +1,41 @@
 package com.openclassrooms.watchlist.service;
 
-public class WatchlistService {
+import com.openclassrooms.watchlist.domain.WatchlistItem;
+import com.openclassrooms.watchlist.exception.DuplicateTitleException;
+import com.openclassrooms.watchlist.repository.WatchlistRepository;
 
+import java.util.List;
+
+public class WatchlistService {
+    WatchlistRepository watchlistRepository = new WatchlistRepository();
+
+    // If the controller class need to call it, then ask it to get from this service.
+    public List<WatchlistItem> getWatchlistItems() {
+        return watchlistRepository.getList();
+    }
+
+    public int getWatchlistItemsSize() {
+        return watchlistRepository.getList().size();
+    }
+
+    public WatchlistItem findWatchlistItemById(Integer id) {
+        return watchlistRepository.findById(id);
+    }
+
+    // If the service class need to call it, then get from repository.
+    public void addOrUpdateWatchlistItem(WatchlistItem watchlistItem) throws DuplicateTitleException {
+        WatchlistItem item = findWatchlistItemById(watchlistItem.getId());
+        if (item == null) {
+            // If it's a new id, then check the given title if it's also new:
+            if (watchlistRepository.findByTitle(watchlistItem.getTitle()) != null) {
+                throw new DuplicateTitleException();
+            }
+            watchlistRepository.addItem(watchlistItem);
+        } else {
+            item.setTitle(watchlistItem.getTitle());
+            item.setRating(watchlistItem.getRating());
+            item.setPriority(watchlistItem.getPriority());
+            item.setComment(watchlistItem.getComment());
+        }
+    }
 }
